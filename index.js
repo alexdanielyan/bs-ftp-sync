c9e5d1ce5ddad9ef2befad903bba1a0f9cada14e
Alexandr Daniyelian <5821262@gmail.com>
2019-08-17 12:12:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { dirname } = require('path');
+const Client = require("ftp");
+const chokidar = require("chokidar");
+
+module.exports.plugin = ftpSync;
+
+function ftpSync(params) {
+    const config = Object.assign({
+        files: '**/*.(php|twig)',
+        watchOptions: {
+            ignored: 'node_modules',
+            ignoreInitial: true,
+        }
+    }, params);
+
+    const client = new Client();
+    
+    client.connect({
+        host: config.host,
+        user: config.user,
+        password: config.password,
+    });
+    
+    function upload(file, event) {
+        client.put(file, config.path + file, function(e) {
+            if (e && e.code === 550) {
+                client.mkdir(dirname(config.path + file), true, function(e) {
+                    if (!e) { upload(file, event); }
+                });
+            } else {
+                const FgGreen = "\x1b[32m";
+                const Reset = "\x1b[0m"
+                console.log(`[${FgGreen}Ftpsync${Reset}] ${file} uploaded`);
+            }
+        });
+    }
+
+    chokidar.watch(config.files, config.watchOptions)
+        .on('add', upload)
+        .on('change', upload);
+}
